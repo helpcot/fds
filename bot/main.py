c07800d2fa22991fc779d7391195cc0740b66ec8
helpcot <62214568+helpcot@users.noreply.github.com>
2020-06-10 15:32:44 +0300

    Add files via upload

---
+++ bot/main.py
@@ -0,0 +1,42 @@
+import discord
+import os
+import random
+import asyncio
+from discord.ext import commands
+
+
+prefix = "/"
+bot = commands.Bot(command_prefix=prefix)
+
+
+
+
+@bot.command() ## Стандартное объявление комманды
+async def load(ctx, extensions): ## объявление функции
+    bot.load_extension(f'cogs.{extensions}')## загрузка доплонений
+    await ctx.send("loaded")
+
+
+@bot.command()
+async def unload(ctx, extensions):
+    bot.unload_extension(f'cogs.{extensions}')
+    await ctx.send("unloaded")
+
+
+@bot.command()
+async def reload(ctx, extensions):
+    bot.unload_extension(f'cogs.{extensions}')# отгружаем ког
+    bot.load_extension(f'cogs.{extensions}')# загружаем
+    await ctx.send('reloaded')
+
+
+
+for filename in os.listdir('./cogs'): # Цикл перебирающий файлы в cogs
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+token = 'Njg5NzU3NDM5OTg5MzgzMjYw.Xt-FLw.PGNkAhyA4D4lkwxY4OueHEkEdsw'
+bot.run(str(token))
+
+
+
